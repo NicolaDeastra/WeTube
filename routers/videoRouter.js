8de1187224c79111a7c80b5e26ab4a1ddff5fb9e
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-09-03 15:02:11 +0700

    set locals and search

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 
 import routes from "../routes";
 import {
-  videos,
   upload,
   videoDetail,
   editVideo,
@@ -11,7 +10,6 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
