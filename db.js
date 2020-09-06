b281667e36977f7a82d6ab3fe44bc9f551a35d1a
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-09-06 15:13:48 +0700

    add videoBlok mixins and setting join router

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 3999,
+    title: "video Awesome",
+    description: "This is cool",
+    views: 99,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 23332,
+      name: "Kahli",
+      email: "kahhi@aov.com",
+    },
+  },
+  {
+    id: 2444,
+    title: "video cool",
+    description: "This is cool",
+    views: 99,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 23332,
+      name: "Kahli",
+      email: "kahhi@aov.com",
+    },
+  },
+  {
+    id: 1222,
+    title: "video Trending",
+    description: "This is cool",
+    views: 99,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 23332,
+      name: "Kahli",
+      email: "kahhi@aov.com",
+    },
+  },
+  {
+    id: 999,
+    title: "video Power Ranger",
+    description: "This is cool",
+    views: 99,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 23332,
+      name: "Kahli",
+      email: "kahhi@aov.com",
+    },
+  },
+];
