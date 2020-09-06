0d3231fa0ea2d91345a6ebae170c4b48f58950f8
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-09-06 20:44:50 +0700

    setting routes and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,10 +17,16 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  res.redirect(routes.home);
+};
 
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
