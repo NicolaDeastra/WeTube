90b06a843a7ff973ae6ce15ca5e4f210c5828691
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-10-04 11:52:32 +0700

    setup upload video and video detail

--- db.js
+++ db.js
@@ -1,14 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/we-tube', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
-})
+});
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ Connected to DB")
-const handelError = (error) => console.log(`❌ Error on DB Connection : ${error}`)
+const handleOpen = () => console.log('✅ Connected to DB');
+const handelError = (error) =>
+  console.log(`❌ Error on DB Connection : ${error}`);
 
-db.once('open', handleOpen)
-db.on('error', handelError)
+db.once('open', handleOpen);
+db.on('error', handelError);
